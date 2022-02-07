770d01c90ab5ecc31ccb83eb98bc408ac57abe6d
shv <sh.veum@gmail.com>
2022-02-07 22:34:28 +0100

    Start of unit & unitTest classes.

---
+++ src/main/java/org/ntnu/wargames/Unit.java
@@ -0,0 +1,53 @@
+package org.ntnu.wargames;
+
+public abstract class Unit {
+    private String name;
+    private int health;
+    private int attack;
+    private int armor;
+
+    public Unit(String name, int health, int attack, int armor) {
+        this.name = name;
+        this.health = health;
+        this.attack = attack;
+        this.armor = armor;
+    }
+
+    public void attack(Unit opponent) {
+        opponent.setHealth(opponent.getHealth() - (this.attack + this.getAttackBonus()) + (opponent.getArmor() + opponent.getResistBonus()));
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public int getAttack() {
+        return attack;
+    }
+
+    public int getArmor() {
+        return armor;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    @Override
+    public String toString() {
+        return "Unit{" +
+                "name='" + name + '\'' +
+                ", health=" + health +
+                ", attack=" + attack +
+                ", armor=" + armor +
+                '}';
+    }
+
+    public abstract int getAttackBonus();
+
+    public abstract int getResistBonus();
+}
---
+++ src/test/java/org/ntnu/wargames/UnitTest.java
@@ -0,0 +1,11 @@
+package org.ntnu.wargames;
+
+import org.junit.Test;
+
+public class UnitTest {
+
+    @Test
+    public void testUnitInput() {
+
+    }
+}
